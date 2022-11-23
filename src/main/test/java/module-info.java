0aae0bc7bca5d3f4a16bfd076eb5346ba9d19a50
Paulo <paulocesarhero@gmail.com>
2022-11-23 10:49:32 -0600

    estructura de pruebas

--- src/main/test/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module test.java.uv.fei {
-    requires junit;
-    requires uv.fei.polinizadores;
-    requires java.sql;
-    requires javafx.base;
-
-    exports uv.fei to junit;
-}
---
+++ src/test/java/uv/fei/PublicacionDAOTest.java
@@ -0,0 +1,21 @@
+package uv.fei;
+
+import org.junit.Before;
+import org.junit.Test;
+import uv.fei.bussinesslogic.PublicacionDAO;
+import uv.fei.domain.Publicacion;
+import static org.junit.Assert.*;
+
+public class PublicacionDAOTest {
+    PublicacionDAO publicacionDAO;
+    Publicacion publicacion;
+    @Before
+    public void init(){
+       // publicacion = new Publicacion("Las abejitas estan en peligro","");
+
+    }
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
